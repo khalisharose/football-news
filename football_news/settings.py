8cc4cf741d38ecb70d4b1e43306e1c93962e2385
khalisharose <khalisharoselani@ui.ac.id>
2025-08-27 17:43:52 +0700

    push kedua

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,9 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 
-...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-...
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khalisha.roselani-footballnews.pbp.cs.ui.ac.id"]
+
 
 
 # Application definition
